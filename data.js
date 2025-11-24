441d176b47e7c605b4ab9c89aa55b0c29ec6d546
Ari Hammond <ajhammond06@gmail.com>
2025-11-24 18:11:46 -0500

    dataset table!! no images

--- data.js
+++ data.js
@@ -13,5 +13,35 @@ fetch('./AIMuseumDataset.json')
 
 function getArtifacts(data) {
     artifacts = data.artifacts;
-    
+    //document.getElementById('artifacts-json').innerHTML = JSON.stringify(artifacts, null,2);
+    let table = '<table style="border-collapse: collapse;">';
+    table += `
+    <thead>
+        <tr>
+            <th>ID </th>
+            <th>Artifact Image </th>
+            <th>Label Text </th>
+            <th>AI Description </th>
+            <th>Human Description </th>
+        </tr>
+    </thead>
+    <tbody>
+    `;
+    for (let i = 0; i < 56; i++) {
+       table += `
+            <tr>
+                <td>${artifacts[i].ID} </td>
+                <td>Image here</td>
+                <td>${artifacts[i].labelText} </td>
+                <td>${artifacts[i].ai_desc} </td>
+                <td>${artifacts[i].human_desc} </td>
+            </tr>
+        `;
+    }
+
+    table += `
+    </tbody>
+    </table>
+    `;
+    document.getElementById("artifact-table").innerHTML = table;
 }
