00e9bd92eb6edbaa883af437df81c4c1c9045a5a
Ari Hammond <ajhammond06@gmail.com>
2026-01-28 11:50:09 -0500

    scaffolding human author half of the dataset

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 let artifacts = null;
+let humanArtifacts = null
 let newDiv, previousDiv;
 
 fetch('./AIMuseumDataset.json')
@@ -10,6 +11,15 @@ fetch('./AIMuseumDataset.json')
             console.error('Error fetching data:', error);
     }
 );
+
+fetch ('./HumanDataset.json')
+    .then(response => response.json())
+    .then(data =>{
+        getHuman(data);
+    })
+    .catch(error => {
+            console.error('Error fetching data:', error);
+    })
 
 function getArtifacts(data) {
     artifacts = data.artifacts;
@@ -44,4 +54,37 @@ function getArtifacts(data) {
     </table>
     `;
     document.getElementById("artifact-table").innerHTML = table;
+}
+
+function getHuman(data) {
+    humanArtifacts = data;
+    //document.getElementById('artifacts-json').innerHTML = JSON.stringify(artifacts, null,2);
+    let table = '<table style="border-collapse: collapse;">';
+    table += `
+    <thead>
+        <tr>
+            <th>ID </th>
+            <th>Artifact Image </th>
+            <th>Label Text </th>
+            <th>Alt Text</th>
+        </tr>
+    </thead>
+    <tbody>
+    `;
+    for (let i = 0; i < humanArtifacts.length; i++) {
+       table += `
+            <tr>
+                <td>${humanArtifacts[i].ID} </td>
+                <td><img src="./Artifact Images/${humanArtifacts[i].image}"/></td>
+                <td>${humanArtifacts[i].labelText} </td>
+                <td>${humanArtifacts[i].altText} </td>
+            </tr>
+        `;
+    }
+
+    table += `
+    </tbody>
+    </table>
+    `;
+    document.getElementById("human-table").innerHTML = table;
 }
